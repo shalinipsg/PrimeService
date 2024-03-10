1bea8dda256f4bddbcdc720951661a4d8a72a4a9
Shalini Cherukuri <52535781+shalinipsg@users.noreply.github.com>
2024-03-09 19:25:08 -0500

    Update primeController.java

--- src/main/java/com/shacheru/primesservice/Controller/primeController.java
+++ src/main/java/com/shacheru/primesservice/Controller/primeController.java
@@ -1,4 +1,20 @@
 package com.shacheru.primesservice.Controller;
+import com.shacheru.primesservice.services.IPrimeService;
 
-public class primeController {
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin
+@RequestMapping("/primes")
+@AllArgsConstructor
+public class PrimeController {
+
+    IPrimeService primeService;
+
+    @GetMapping("/{n}")
+    public boolean isPrime(@PathVariable long n) {
+        return primeService.isPrime(n);
+    }
+
 }
