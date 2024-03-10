9b629480422d0c42c8863d7acb865f1033c168ac
shacheru <shacheru@iu.edu>
2024-03-09 21:27:19 -0500

    added service

--- src/main/java/com/shacheru/primesservice/Controller/primeController.java
+++ src/main/java/com/shacheru/primesservice/Controller/primeController.java
@@ -2,14 +2,15 @@ package com.shacheru.primesservice.Controller;
 import com.shacheru.primesservice.Service.IPrimeService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 @RequestMapping("/primes")
-@AllArgsConstructor
-public class primeController {
 
+public class primeController {
+@Autowired
     IPrimeService primeService;
 
     @GetMapping("/{n}")
