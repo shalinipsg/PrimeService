e1a005b77f1f70e005030feb1a33c0dd26e547fe
kundhana2123 <kundhana02@gmail.com>
2024-03-28 19:40:48 -0400

    "Updated Authentication controller and token service"

--- src/main/java/com/shacheru/primesservice/Controller/AuthenticationController.java
+++ src/main/java/com/shacheru/primesservice/Controller/AuthenticationController.java
@@ -13,44 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-//@RestController
-//public  class AuthenticationController {
-//    private TokenService tokenService;
-////    private AuthenticationManager authenticationManager;
-//    private final IAuthenticationService authenticationService;
-////@Autowired
-//    public AuthenticationController(IAuthenticationService authenticationService, AuthenticationManager authenticationManager, TokenService tokenService){
-//        this.authenticationService = authenticationService;
-//        this.authenticationManager = authenticationManager;
-//        this.tokenService = tokenService;
-//    }
-//    @PostMapping("/register")
-//
-//    public boolean register(@RequestBody Customer customer) {
-//
-//        try {
-//            System.out.print("jhgmn");
-//            return authenticationService.register(customer);
-//        } catch (IOException e) {
-//
-//            throw new RuntimeException(e);
-//
-//        }
-//
-//    }
-//    @PostMapping("/login")
-//
-//    public String login (@RequestBody Customer customer) {
-//        Authentication authentication = authenticationManager.authenticate(
-//        new UsernamePasswordAuthenticationToken( customer.getUsername() , customer.getPassword()));
-//        return tokenService.generateToken(authentication);
-//    }
-//    private final AuthenticationManager authenticationManager;
-//    public AuthenticationController(AuthenticationManager authenticationManager,IAuthenticationService authenticationService){
-//        this.authenticationManager = authenticationManager;
-//        this.authenticationService = authenticationService;
-//    }
-//}
 
 @RestController
 public class AuthenticationController {
